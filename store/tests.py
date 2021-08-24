6c4a95acca6025fd960beb612adcea5988a84e9d
Samson Wandah <samsonwandah@gmail.com>
2021-08-24 17:24:44 +0300

    urls and views

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from store.models import Category, Product
 
 # Create your tests here.
+class TestCategoriesModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django',slug='django')
+
+    def test_category_model_entry(self):
+        """"
+        Test Category model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data,Category))
+
+    def test_category_model_entry(self):
+        """"
+        Test Category model default name
+        """
+        data = self.data1
+        self.assertEqual(str(data),'django')
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='Mobile',slug='mobile')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1,title='Apple Phone',created_by_id=1,slug='apple-phone',price='20.00',image='image')
+
+    def test_products_model_entry(self):
+        """"
+        Test Product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data,Product))
+        self.assertEqual(str(data),'Apple Phone')
